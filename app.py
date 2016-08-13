a275e4c27ddceb9d3cf764f2cf8d53e3bfd661ab
drupesh97 <drupesh97@gmail.com>
2016-08-13 18:38:08 +0530

    Inputs message from FB

--- app.py
+++ app.py
@@ -1,17 +1,35 @@
 from flask import Flask
+from flask import request
+from wit import Wit
+from fireb import bookReservations
+
+import json
+import requests
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route('/', methods=['POST','GET'])
+def h():
+	#return request.args.get('hub.challenge')
+	#return 'Starting'	
+	#a=json.loads(request.get_json())
+	#print a
+	a = request.get_json()
+	print type(a)
+	print ">>>>>>>>>>>>>>>>>>>."
+	FBm = a['entry'][0]['messaging']['0']['message']['text']
+	return 'asdas'
+	'''Wit PUT
+	mains(FBm)x`
+	'''
 
-@app.route('/receive')
-def receive():
-    """
-    Expecting:
-    {"message": ""}
-    """
-    json.loads(req.body)
-    print('Contacting wit.ai...')
-    print('Got from wit.ai')
-    return 'This is a response from wit.ai'
+@app.route('/receive',methods=['POST','GET'])
+def g():
+	#return request.args.get('hub.challenge')
+	a = request.get_json()
+	try:
+		mess = a['entry'][0]['messaging'][0]['message']['text']
+		#bookReservations()
+	except Error:
+		print ('Facebook message not obtained')
+
+	return 'hello world'
---
+++ fireb.py
@@ -0,0 +1,4 @@
+from firebase import firebase
+firebase = firebase.FirebaseApplication('https://emily-c3113.firebaseio.com', None)
+
+def bookReservations():
