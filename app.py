bbf82da93ab8100edb39d758c7a4cb2b1e830294
drupesh97 <drupesh97@gmail.com>
2016-08-13 22:58:33 +0530

    FB msg works

--- app.py
+++ app.py
@@ -1,35 +1,59 @@
 from flask import Flask
 from flask import request
 from wit import Wit
-from fireb import bookReservations
-
 import json
 import requests
+import os
 app = Flask(__name__)
-
-@app.route('/', methods=['POST','GET'])
-def h():
-	#return request.args.get('hub.challenge')
-	#return 'Starting'	
-	#a=json.loads(request.get_json())
-	#print a
-	a = request.get_json()
-	print type(a)
-	print ">>>>>>>>>>>>>>>>>>>."
-	FBm = a['entry'][0]['messaging']['0']['message']['text']
-	return 'asdas'
-	'''Wit PUT
-	mains(FBm)x`
-	'''
 
 @app.route('/receive',methods=['POST','GET'])
 def g():
-	#return request.args.get('hub.challenge')
 	a = request.get_json()
+	#print a
+	#print ">>>>>>>>>>>>>>>>>>>"
 	try:
 		mess = a['entry'][0]['messaging'][0]['message']['text']
-		#bookReservations()
-	except Error:
-		print ('Facebook message not obtained')
+		print mess
+		userID= a['entry'][0]['messaging'][0]['sender']['id']
+		#postFbtext(userID,)
+		#witResp(mess)
+	except:
+		print 'FB message input Failed'
 
 	return 'hello world'
+
+Witaccess_token = "LVLKPQNZ5IEMVZ6OB5QHGSUZGBBGZCNM"
+
+def witResp(message):
+	try:
+		client = Wit(access_token=Witaccess_token)
+		resp = client.message(message)
+	except:
+		print 'Error in wit message() API'
+
+def getJson(userID):
+	a={
+	  "recipient":{
+	    "id": str(userID)
+	  },
+	  'message':{
+	    "text":"hello, world!"
+	  }
+	}
+	return a
+
+def postFbtext(userID,msg):
+	try:
+		string ="curl -X POST -H \"Content-Type: application/json\" -d \'{ \
+	  \"recipient\":{	\
+	    \"id\":\"" + str(userID) + "\" \
+	  },	\
+	  \"message\":{ \
+	    \"text\":\"" + str(msg) +"\" \
+	  } \
+	}' \"https://graph.facebook.com/v2.6/me/messages?access_token=EAAD5596IWWYBABpdG4yEChjuvAqU2HQ0xZBYDo48BXrXPBTvcU6yUeXplDdPA63YVGPl7eIBr2AH2sfDwaouq6mkrpNn1VVueP0pHin9s81S2wQtJvveZA3G6fr7NXBKtKHUlLFX2PMzlT5Y8K14q39bHRCh6pjGReZA8IKlQZDZD\""
+		os.system(string)
+	except:
+		print 'Error in Posting to FB'
+
+	
--- fireb.py
+++
@@ -1,4 +0,0 @@
-from firebase import firebase
-firebase = firebase.FirebaseApplication('https://emily-c3113.firebaseio.com', None)
-
-def bookReservations():
